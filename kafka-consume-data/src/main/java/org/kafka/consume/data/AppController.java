066581db83975db3e3c76781ef040a0fc34cf0ef
Deepshika <deepshika@Deepshikas-MacBook-Air.local>
2023-04-13 00:25:08 -0400

    new api added

--- kafka-consume-data/src/main/java/org/kafka/consume/data/AppController.java
+++ kafka-consume-data/src/main/java/org/kafka/consume/data/AppController.java
@@ -4,9 +4,11 @@ import org.kafka.consume.data.entity.StreamData;
 import org.kafka.consume.data.repository.MediaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AppController {
@@ -17,5 +19,10 @@ public class AppController {
     @RequestMapping("/fetchEventData")
     public List<StreamData> fetchEventData() {
         return dataRepository.findAll();
+    }
+    
+    @RequestMapping("/fetchEventById")
+    public Optional<StreamData> fetchEventDataById(@RequestParam() Long id) {
+        return dataRepository.findById(id);
     }
 }
